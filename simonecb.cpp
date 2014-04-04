fd6334601a68553b979efb1ab1a74f0608ae3a39
Zane Salem <zsalem@umich.edu>
2014-04-04 14:10:35 -0400

    organized code

--- simonecb.cpp
+++ simonecb.cpp
@@ -4,51 +4,9 @@
 
 const int KEY_SIZE_BITS = 256;
 
-
 using namespace std;
 
-
-
-
-uberzahl genKey() {
-	unsigned long n = KEY_SIZE_BITS;	
-
-	default_random_engine RNG;
-	RNG.seed(time(NULL));
-	bernoulli_distribution bTrial(.5);
-
-	uberzahl key = "0";
-	for(unsigned long i = 0; i < n; i += 1) {
-		if (bTrial(RNG))
-			key.setBit(i);
-	}
-
-	return key;
-
-}
-
-
-
-
-uberzahl notOperation(const uberzahl input) {
-	return ((one << n) - 1) ^ input;
-}
-
-void printB(const uberzahl input, int numBits){
-	for(int i = numBits - 1; i >= 0; i--){
-		cout << input.bit(i);
-		if(i%4 == 0){
-			cout << " ";
-		}
-	}
-	cout << endl;
-}
-
-void keyExpansion() {
-
-
-}
-
+/* crypto functions */
 vector<uberzahl> encrypt(vector<uberzahl> msg, uberzahl key) {
 
     uberzahl k[T];
@@ -80,27 +38,25 @@ vector<uberzahl> encrypt(vector<uberzahl> msg, uberzahl key) {
 		//k[i] = c ^ z[j].bit(61 - ((i-m) % 62)) ^ k[i-m] ^ tmp ^ tmp.rotateRight(1,0,n-1);
 	}
 
-
-
     vector<uberzahl> ciphertext;
 
     int size = msg.size();
     for(int l = 0; l < size; ++l) {
 
-	    uberzahl x = msg[l] >> n; //first half
-	    uberzahl y = msg[l] & ((one << n) - 1); //second half
-
-	    for (int i = 0; i < T; i++){
-		    uberzahl tmp = x;
-		    x = y ^ ((x.rotateLeft(1,0,n-1) & x.rotateLeft(8,0,n-1)) ^ x.rotateLeft(2,0,n-1)) ^ k[i];
-		    y = tmp;
-	    }
-
-	    uberzahl cipher_msg = y + (x << n);
+	uberzahl x = msg[l] >> n; //first half
+	uberzahl y = msg[l] & ((one << n) - 1); //second half
+	
+	for (int i = 0; i < T; i++){
+	    uberzahl tmp = x;
+	    x = y ^ ((x.rotateLeft(1,0,n-1) & x.rotateLeft(8,0,n-1)) ^ x.rotateLeft(2,0,n-1)) ^ k[i];
+	    y = tmp;
+	}
+	
+	uberzahl cipher_msg = y + (x << n);
         ciphertext.push_back(cipher_msg);
     }
 
-	return ciphertext;
+    return ciphertext;
 }
 
 vector<uberzahl> decrypt(vector<uberzahl> cipher_msg, uberzahl key) {
@@ -154,7 +110,37 @@ vector<uberzahl> decrypt(vector<uberzahl> cipher_msg, uberzahl key) {
 	return plaintext;
 }
 
+/* helper functions */
+uberzahl genKey() {
+	unsigned long n = KEY_SIZE_BITS;	
 
+	default_random_engine RNG;
+	RNG.seed(time(NULL));
+	bernoulli_distribution bTrial(.5);
+
+	uberzahl key = "0";
+	for(unsigned long i = 0; i < n; i += 1) {
+		if (bTrial(RNG))
+			key.setBit(i);
+	}
+
+	return key;
+}
+
+uberzahl notOperation(const uberzahl input) {
+	return ((one << n) - 1) ^ input;
+}
+
+/* debugging functions */
+void printB(const uberzahl input, int numBits){
+	for(int i = numBits - 1; i >= 0; i--){
+		cout << input.bit(i);
+		if(i%4 == 0){
+			cout << " ";
+		}
+	}
+	cout << endl;
+}
 
 string getHex(uberzahl u) {	
 	string out = "";
@@ -169,6 +155,3 @@ string getHex(uberzahl u) {
 	}
 	return out;
 }
-
-
-
