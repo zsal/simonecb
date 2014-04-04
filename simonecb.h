6ef0ae06c933a5f8b66ee613a1487d64ccbd7ed4
Zane Salem <zsalem@umich.edu>
2014-04-04 14:11:16 -0400

    Update simonecb.h

--- simonecb.h
+++ simonecb.h
@@ -13,12 +13,9 @@ const unsigned int m = 4; //# of key words
 const unsigned int T = 72; //# of rounds
 const unsigned int j = 4; //const sequence of z
 
+const uberzahl zero = "0";
 const uberzahl one  = "1";
 const uberzahl two  = "2";
-const uberzahl zero = "0";
-
-
-
 
 // z onstant
 const uberzahl z[5] = {4506230155203752166,
@@ -27,21 +24,13 @@ const uberzahl z[5] = {4506230155203752166,
                        3957284701066611983, 
                        3781244162168104175};
 
-
-
 /* utility */
 uberzahl notOperation(const uberzahl input);
 void printB(const uberzahl input, int numBits);
 std::string getHex(uberzahl u);
 
-
-
-
-
 /* Enc / Dec */
 uberzahl genKey();
-
-void keyExpansion();
 
 std::vector<uberzahl> encrypt(std::vector<uberzahl> msg, uberzahl key);
 std::vector<uberzahl> decrypt(std::vector<uberzahl> cipher_msg, uberzahl key);
